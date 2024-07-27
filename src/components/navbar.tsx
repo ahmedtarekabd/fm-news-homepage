7d344a52c79d3ffedc112dcddd66b43ae00dc63b
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2024-07-27 15:51:30 +0300

    Setup the project's dependencies.

---
+++ src/components/navbar.tsx
@@ -0,0 +1,25 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+
+const Navbar = () => {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+export default Navbar;
