f0c3730de8ae21cd7137d7b99331f345acb8a35e
Ahmed Tarek <ahmedtarekabd2002@gmail.com>
2024-07-27 15:56:01 +0300

    Linked favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/assets/images/favicon-32x32.png"
+          type="image/png"
+        />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
